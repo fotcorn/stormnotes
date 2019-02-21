8f83de95d17cd4dfff4ff15a602dc948050daf66
fotcorn <fotcorn@gmail.com>
2019-02-21 22:48:35 +0100

    backend: correctly setup authentication configuration

--- backend/stormnotes/settings.py
+++ backend/stormnotes/settings.py
@@ -123,7 +123,10 @@ REST_FRAMEWORK = {
     # all api reviews require authentication by default
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    )
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 SIMPLE_JWT = {
