54273e791406af65270fffbd58c437f9b72c71d7
fotcorn <fotcorn@gmail.com>
2019-02-22 11:07:30 +0100

    frontend: add type definition for markdown-it sub and sup

--- frontend/src/index.d.ts
+++ frontend/src/index.d.ts
@@ -1,3 +1,5 @@
-declare module 'markdown-it-highlightjs';
 declare module 'vue-codemirror';
+declare module 'markdown-it-highlightjs';
 declare module 'markdown-it-katex';
+declare module 'markdown-it-sub';
+declare module 'markdown-it-sup';
