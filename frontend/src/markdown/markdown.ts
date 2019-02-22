55e69c83c146d9515981e93a2092196a259cf6f6
fotcorn <fotcorn@gmail.com>
2019-02-22 10:21:27 +0100

    frontend: add sub and supscript support

--- frontend/src/markdown/markdown.ts
+++ frontend/src/markdown/markdown.ts
@@ -1,6 +1,8 @@
 import markdownIt from 'markdown-it';
 import highlightjs from 'markdown-it-highlightjs';
 import checkbox from './markdown-it-checkbox';
+import sub from 'markdown-it-sub';
+import sup from 'markdown-it-sup';
 import katex from 'markdown-it-katex';
 import link from './markdown-it-link';
 
@@ -8,6 +10,8 @@ const md = markdownIt({ breaks: true, linkify: true })
   .use(highlightjs)
   .use(checkbox)
   .use(link)
+  .use(sub)
+  .use(sup)
   .use(katex);
 
 export default function render(markdown: string, currentPage: string): string {
